44d765043b42ebb25182fe314656833c7257e926
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2025-01-06 15:53:18 -0500

    Actualización de los splash screens

--- app/src/main/java/com/ericksebas03/realidadaumentadamuseo/MainActivity.kt
+++ app/src/main/java/com/ericksebas03/realidadaumentadamuseo/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.ericksebas03.realidadaumentadamuseo
 
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.ericksebas03.splashscreen.SplashActivity
+import com.ericksebas03.splashscreen.Splash2Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnIrA = findViewById<Button>(R.id.btn_1)
+        val btnIrA = findViewById<Button>(R.id.btn_acceder) // Verifica que btn_acceder existe en el XML
 
         btnIrA.setOnClickListener {
-            // Crear un Intent para iniciar SplashActivity
-            val intent = Intent(this, SplashActivity::class.java)
-            // Iniciar SplashActivity
+            // Crear un Intent para iniciar Splash2Activity
+            val intent = Intent(this, Splash2Activity::class.java)
+            // Iniciar Splash2Activity
             startActivity(intent)
         }
     }
