55fbe70fd482dc73219eeac8414ec12e62d8358d
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2025-01-06 15:53:18 -0500

    Actualización de los splash screens

---
+++ app/src/main/java/com/ericksebas03/splashscreen/Splash2Activity.kt
@@ -0,0 +1,26 @@
+package com.ericksebas03.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.ericksebas03.realidadaumentadamuseo.R
+import com.ericksebas03.adivinanzas.Adivinanza1Activity
+
+class Splash2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+
+        // Buscar el botón en el layout
+        val botonContinuar = findViewById<Button>(R.id.bottonsplash2)
+
+        // Configurar el click del botón
+        botonContinuar.setOnClickListener {
+            // Crear un Intent para iniciar Adivinanza1Activity
+            val intent = Intent(this, Adivinanza1Activity::class.java)
+            // Iniciar Adivinanza1Activity
+            startActivity(intent)
+        }
+    }
+}
