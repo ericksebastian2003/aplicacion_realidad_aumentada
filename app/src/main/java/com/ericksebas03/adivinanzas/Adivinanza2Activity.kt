f0a0b028958e103191bb1777f63bb0e2d014f3b3
Eduardo-Caza <ecaanaranjo@gmail.com>
2025-01-12 18:59:14 -0500

    añadido de preguntas biodiversidad amazonica

---
+++ app/src/main/java/com/ericksebas03/adivinanzas/Adivinanza2Activity.kt
@@ -0,0 +1,12 @@
+package com.ericksebas03.adivinanzas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ericksebas03.realidadaumentadamuseo.R
+
+class Adivinanza2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adivinanza2)
+    }
+}
